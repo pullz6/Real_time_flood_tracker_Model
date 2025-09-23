45689b405ebdf2fa4a71ca35a003c34488786ec0
Sara <gunawardhanapulsara@gmail.com>
2025-09-23 19:42:25 +0100

    preprocess started

--- preprocess.py
+++ preprocess.py
@@ -5,8 +5,7 @@ import os
 current_directory = os.getcwd()
 file_path = os.path.join(current_directory, 'flood_data', 'flood_warnings.csv')
 df = pd.read_csv(file_path)
-print(df.head())
+
 
 file_path = os.path.join(current_directory, 'flood_data', 'stations.csv')
 df_stations = pd.read_csv(file_path)
-print(df_stations.head())
